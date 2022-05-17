bef2e3fd91b3fff428f216cb46dba62ecfca6eef
ramirolynch <ramirolynch@yahoo.com>
2022-05-16 21:21:39 -0500

    created crud operations for posts and comments

--- src/routes/postsroutes.ts
+++ src/routes/postsroutes.ts
@@ -93,7 +93,7 @@ routes.post("/posts/:id/comments", (req, res) => {
   const comment = {
     body: req.body.body,
     author_id: req.body.author_id,
-    post_id: req.params.id,
+    post_id: +req.params.id,
   };
   db.one(
     "INSERT INTO comments(body, author_id, post_id) VALUES(${body}, ${author_id}, ${post_id}) returning id",
@@ -106,6 +106,46 @@ routes.post("/posts/:id/comments", (req, res) => {
     })
     .then((data) => res.json(data))
     .catch((error) => res.status(500).send(error));
+});
+
+// edit comment
+routes.put("/posts/comments/:id", (req, res) => {
+  db.many("select * from comments")
+    .then((comm) => {
+      let elem: any = comm.find((c) => c.id === +req.params.id);
+
+      if (!elem) {
+        res.status(404).json({ error: "Comment not found" });
+      } else {
+        db.none("update comments set body=${body} where id = ${id}", {
+          id: +req.params.id,
+          body: req.body.body,
+        });
+        res.send(req.body);
+      }
+    })
+    .catch((error) => console.log(error));
+});
+
+// delete comment
+routes.delete("/posts/comments/:id", (req, res) => {
+  db.many("select * from comments")
+    .then((elements) => {
+      let elem: any = elements.find((e) => e.id === +req.params.id);
+
+      if (!elem) {
+        res.status(404).json({ error: "Comment not found" });
+      } else {
+        db.none("delete from comments where id = ${id}", {
+          id: +req.params.id,
+        });
+
+        res
+          .status(200)
+          .json({ message: `Post with id ${+req.params.id} deleted` });
+      }
+    })
+    .catch((error) => console.log(error));
 });
 
 // get a single post by the id
@@ -132,15 +172,13 @@ routes.get("/posts/:id/comments", (req, res) => {
     .catch((error) => console.log(error));
 });
 
-// routes.get("/comments/:id", (req, res) => {
-//   db.oneOrNone(
-//     "select posts.id,posts.title,posts.body,posts.post_ts, posts.author_id, users.first_name, users.last_name, users.admin, users.email from posts join users on posts.author_id = users.id where posts.id = ${id}",
-//     {
-//       id: req.params.id,
-//     }
-//   )
-//     .then((data) => res.json(data))
-//     .catch((error) => console.log(error));
-// });
+// get a single comment by its id
+routes.get("/comments/:id", (req, res) => {
+  db.oneOrNone("select * from comments where comments.id = ${id}", {
+    id: req.params.id,
+  })
+    .then((data) => res.json(data))
+    .catch((error) => console.log(error));
+});
 
 export default routes;
